9c734c4603cc8fcc3fb0e440f0618be053031c6f
jaimin25 <chovatiajaimin@gmail.com>
2024-11-25 22:46:01 +0530

    fix: site url

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const APP = {
   NAME: 'GreatMinds',
   DESCRIPTION:
     "Explore great individuals who revolutionized the world with their skills and knowledge, shaping humanity's progress through groundbreaking contributions and innovations.",
-  URL: 'https://grtminds.vercel.app',
+  URL: 'https://grtminds.xyz/',
 };
